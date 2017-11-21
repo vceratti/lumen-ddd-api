b55a11137bb7917c8fecd97465ac9e8d5fae258c
vceratti <v.ceratti@gmail.com>
2017-11-21 18:46:54 -0200

    Incomplete rest structure update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # lumen-ddd-api
 
 REST Api structure using Lumen 5.4 and Domain Driven Design Concepts
+
+
+
+### Mantainer  ###
+
+Vinícius Ceratti
+
+_v.ceratti@gmail.com_ |  _linkedin.com/in/vceratti_ | _Skype: ceratti_
--- new-api-project.sh
+++ new-api-project.sh
@@ -14,7 +14,7 @@ Possible parameters:
     exit 1
 }
 
-libVersion="1.0.2"
+libVersion="1.0.3"
 
 function importLib {
     removeLib
@@ -39,24 +39,38 @@ function install_php_build {
     if should_run_command "Install development environment?    This will checkout build files and install some tools (like Ant and Docker)"; then
         checkout_build_repo
 
-        cd "build"
+        cd ".build"
         chmod +x install-php-build.sh
         ./install-php-build.sh --project-name="$project_name"
-
-        mv  docker-compose.yml "$root/docker-compose.yml"
-        mv  php-build "$root/php-build"
 
         cd "$root"
     fi
 }
 
+function create_tools_scripts {
+    log "Creating Artisan and Composer executables"
+    echo "#!/usr/bin/env bash" > ./artisan && \
+    echo "docker-compose up -d" >> ./artisan
+    echo "docker exec -it ${project_name}_php_build php ./src/artisan \$@" >> ./artisan
+    printf "docker exec -it ${project_name}_php_build chown -R " >> ./artisan
+    printf '`stat -c "%%u:%%g" .` .' >> ./artisan
+
+    echo "#!/usr/bin/env bash" > ./composer && \
+    echo "docker-compose up -d" >> ./composer
+    echo "docker exec -it ${project_name}_php_build composer \$@" >> ./composer
+    printf "docker exec -it ${project_name}_php_build chown -R " >> ./composer
+    printf '`stat -c "%%u:%%g" .` ./vendor' >> ./composer
+
+    sudo chmod +x ./artisan ./composer
+
+}
 function composer_update {
     if should_run_command "Run composer update?"; then
         checkout_build_repo
-        cd build
+        cd .build
         chmod +x install.sh
         exec ./install.sh
-        php="docker exec -it php-build7.1 php -i"
+        php="docker exec -it php_build7.1 php -i"
         cd "$root"
     fi
 
@@ -68,10 +82,10 @@ function clean {
 
     sudo ls &> /dev/null
 
-    log_wait "Cleaning old files (./mysql /build /vendor)"
+    log_wait "Cleaning old files (./.mariadb /.build /vendor)"
 
-    sudo rm -rf build .mysql vendor &> /dev/null
-    sudo rm docker-php-build-up.sh docker-compose.yml php-build &> /dev/null
+    sudo rm -rf build .mariadb vendor &> /dev/null
+    sudo rm docker-php_build-up.sh docker-compose.yml ant-build composer.lock artisan composer &> /dev/null
 
     log_done
 }
@@ -85,6 +99,7 @@ function main {
     choose_env
 
     install_php_build
+    create_tools_scripts
     removeLib
 
 }
